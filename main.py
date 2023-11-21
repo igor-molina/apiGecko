6229d5d3b707c7dda30fb3c28b1bc2c990a8f2b3
unknown <igordsmolina@gmail.com>
2023-11-21 11:16:17 -0300

    Create id list and supported vs currencies list

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+
+url = 'https://api.coingecko.com/api/v3/'
+
+
+def get_coin_list_id():
+    r = requests.get(url+"coins/list")
+    list_coins = r.json()
+    id_list = [coin['id'] for coin in list_coins[:1000]]
+    return ",".join(id_list)
+
+
+def supported_vs_currencies():
+    r = requests.get(url+"simple/supported_vs_currencies")
+    svc_list = r.json()
+    svc_list = svc_list[:1000]
+    return ",".join(svc_list)
+
+
+def get_coin_markets(svc, id_list):
+    payload = {"vs_currency": svc, "ids": id_list}
+    r = requests.get(url + "coins/markets", params=payload)
+
+    try:
+        r.raise_for_status()  # Verificar se houve algum erro na requisição
+        data = r.json()
+        print(data)
+    except requests.exceptions.HTTPError as errh:
+        print("HTTP Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("Ocorreu um erro durante a requisição:", err)
+
+
+svc_string = supported_vs_currencies()
+coins_id_list = get_coin_list_id()
+#print(coins_id_list)
+get_coin_markets(svc_string, coins_id_list)
